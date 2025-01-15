cdb08dbd36b7bcf3f2a94209c273985e41884b62
dannyKBjj <daniel.king1978@gmail.com>
2025-01-15 17:27:27 +0000

    Seems we cannot update
    https://learn.microsoft.com/en-us/answers/questions/2124206/how-to-use-update-mgdevicemanagementdevicecomplian

--- Modules/Microsoft365DSC/DSCResources/MSFT_IntuneDeviceCompliancePolicyAndroidWorkProfile/MSFT_IntuneDeviceCompliancePolicyAndroidWorkProfile.psm1
+++ Modules/Microsoft365DSC/DSCResources/MSFT_IntuneDeviceCompliancePolicyAndroidWorkProfile/MSFT_IntuneDeviceCompliancePolicyAndroidWorkProfile.psm1
@@ -504,7 +504,6 @@ function Set-TargetResource
         [ValidateSet('basic', 'hardwareBacked')]
         $SecurityRequiredAndroidSafetyNetEvaluationType,
 
-
         [Parameter()]
         [Microsoft.Management.Infrastructure.CimInstance[]]
         $Assignments,
@@ -542,7 +541,7 @@ function Set-TargetResource
         [System.String[]]
         $AccessTokens
     )
-
+    
     Write-Verbose -Message "Intune Android Work Profile Device Compliance Policy {$DisplayName}"
 
     $ConnectionMode = New-M365DSCConnection -Workload 'MicrosoftGraph' `
@@ -559,59 +558,58 @@ function Set-TargetResource
         -Parameters $PSBoundParameters
     Add-M365DSCTelemetryEvent -Data $data
     #endregion
-
+    
     $currentDeviceAndroidPolicy = Get-TargetResource @PSBoundParameters
-
+    
+    
+    <#
     $PSBoundParameters.Remove('Ensure') | Out-Null
     $PSBoundParameters.Remove('Credential') | Out-Null
     $PSBoundParameters.Remove('ApplicationId') | Out-Null
     $PSBoundParameters.Remove('TenantId') | Out-Null
     $PSBoundParameters.Remove('ApplicationSecret') | Out-Null
     $PSBoundParameters.Remove('AccessTokens') | Out-Null
-    <#
-    $scheduledActionsForRule = @{
-        '@odata.type'                 = '#microsoft.graph.deviceComplianceScheduledActionForRule'
-        ruleName                      = 'PasswordRequired'
-        scheduledActionConfigurations = @(
-            @{
-                '@odata.type' = '#microsoft.graph.deviceComplianceActionItem'
-                actionType    = 'block'
-            }
-        )
-    }
     #>
-#reconstruct scheduled action configurations for use with New/Update-MgBetaDeviceManagementDeviceCompliancePolicy
-$PSBoundParameters.scheduledActionsForRule.CimInstanceProperties | Out-File "C:\dsc-5592\CimInstanceProperties.txt"
+    
+    $PSBoundParameters = Remove-M365DSCAuthenticationParameter -BoundParameters $PSBoundParameters
+    #reconstruct scheduled action configurations for use with New/Update-MgBetaDeviceManagementDeviceCompliancePolicy
+    
+    $allTargetValues = Convert-M365DscHashtableToString -Hashtable $PSBoundParameters 
+    
+    $PSBoundParameters.Remove('scheduledActionsForRule') | Out-Null
+    
+    if ($allTargetValues -match '\bscheduledActionsForRule=\(\{([^\)]+)\}\)') 
+    {    
+        $scheduledActionConfigurations = @() #define array we'll be adding hashtables to      
+        $scheduledActionsForRuleBlock = $matches[1] -split "," -replace "[{}]"        
+        foreach($item in $scheduledActionsForRuleBlock){
+            $array = $item -split ";"
+            $array = $array | Where-Object { $_ -notlike "id=*" } #drop id we don't want it  
+            $hashtable = @{}              
+            foreach ($item in $array) {
+                
+                if ($item -match "^(.*?)=(.*)$") 
+                {
+                    $key = $matches[1].Trim()
+                    $value = $matches[2].Trim()
+                    $hashtable.Add($key,$value)
+                }         
+            }
+            $scheduledActionConfigurations += $hashtable           
+        }                       
+    }
 
-foreach ($property in $PSBoundParameters.scheduledActionsForRule.CimInstanceProperties) {
-        $scheduledActionConfigurations = @()   
-        $hashtable = @{}
-        $hashtable.Add('actionType', $property.actionType)
-        $hashtable.Add('gracePeriodHours', [int]$property.gracePeriodHours)
-        $hashtable.Add('notificationMessageCcList', [Array]$property.notificationMessageCcList)
-        $hashtable.Add('notificationTemplateId', $property.notificationTemplateId)
 
-        $scheduledActionConfigurations += $hashtable  
-         <#    
-        foreach($item in $property.Value){
-            $hashtable = @{}
-            $hashtable.Add('actionType', $item.actionType)
-            $hashtable.Add('gracePeriodHours', [int]$item.gracePeriodHours)
-            $hashtable.Add('notificationMessageCcList', [Array]$item.notificationMessageCcList)
-            $hashtable.Add('notificationTemplateId', $item.notificationTemplateId)
 
-            $scheduledActionConfigurations += $hashtable  
-            $scheduledActionConfigurations | Out-File "C:\dsc-5592\HASH.txt"
-        }
-        #>
-}
-$scheduledActionsForRule = @{
-    '@odata.type'                 = '#microsoft.graph.deviceComplianceScheduledActionForRule'
-    ruleName                      = '' #this is always blank and can't be set in GUI
-    scheduledActionConfigurations = $scheduledActionConfigurations
-}
-$scheduledActionsForRule | Out-File "C:\dsc-5592\scheduledActionsForRuleBROKE.txt"
+    Write-Verbose -Message "CODE ABOVE RULED OUT"  
 
+    $myScheduledActionsForRule = @{
+        '@odata.type'                 = '#microsoft.graph.deviceComplianceScheduledActionForRule'
+        ruleName                      = '' #this is always blank and can't be set in GUI
+        scheduledActionConfigurations = $scheduledActionConfigurations
+    }
+    Write-Verbose -Message "CODE BELOW RULED OUT"
+    
     if ($Ensure -eq 'Present' -and $currentDeviceAndroidPolicy.Ensure -eq 'Absent')
     {
         Write-Verbose -Message "Creating new Intune Android Work Profile Device Compliance Policy {$DisplayName}"
@@ -623,7 +621,7 @@ $scheduledActionsForRule | Out-File "C:\dsc-5592\scheduledActionsForRuleBROKE.tx
         $policy = New-MgBetaDeviceManagementDeviceCompliancePolicy -DisplayName $DisplayName `
             -Description $Description `
             -AdditionalProperties $AdditionalProperties `
-            -ScheduledActionsForRule $scheduledActionsForRule
+            -ScheduledActionsForRule $myScheduledActionsForRule
 
         #region Assignments
         $assignmentsHash = ConvertTo-IntunePolicyAssignment -IncludeDeviceFilter:$true -Assignments $Assignments
@@ -874,97 +872,6 @@ function Test-TargetResource
         [System.String[]]
         $AccessTokens
     )
-<#
-    #Ensure the proper dependencies are installed in the current environment.
-    Confirm-M365DSCDependencies
-
-    #region Telemetry
-    $ResourceName = $MyInvocation.MyCommand.ModuleName -replace 'MSFT_', ''
-    $CommandName = $MyInvocation.MyCommand
-    $data = Format-M365DSCTelemetryParameters -ResourceName $ResourceName `
-        -CommandName $CommandName `
-        -Parameters $PSBoundParameters
-    Add-M365DSCTelemetryEvent -Data $data
-    #endregion
-    Write-Verbose -Message "Testing configuration of Intune Android Work Profile Device Compliance Policy {$DisplayName}"
-
-    $CurrentValues = Get-TargetResource @PSBoundParameters
-    if (-not (Test-M365DSCAuthenticationParameter -BoundParameters $CurrentValues))
-    {
-        Write-Verbose "An error occured in Get-TargetResource, the policy {$displayName} will not be processed"
-        throw "An error occured in Get-TargetResource, the policy {$displayName} will not be processed. Refer to the event viewer logs for more information."
-    }
-
-    Write-Verbose -Message "Current Values: $(Convert-M365DscHashtableToString -Hashtable $CurrentValues)"
-    Write-Verbose -Message "Target Values: $(Convert-M365DscHashtableToString -Hashtable $PSBoundParameters)"
-
-    $ValuesToCheck = $PSBoundParameters
-
-#EXPERMIMENTS
-$Temp = $PSBoundParameters.scheduledActionsForRule
-foreach ($property in $PSBoundParameters.scheduledActionsForRule.CimInstanceProperties) {
-    If($property.Name -eq 'scheduledActionConfigurations')
-    {
-        $property.Value.CimInstanceProperties | Out-File -FilePath "C:\dsc-5592\$($property.Name).txt"
-        $property | Out-File -FilePath "C:\dsc-5592\properties.txt"
-        $property.Value[0].actionType | Out-File -FilePath "C:\dsc-5592\properties.txt"
-    }
-}
-$CurrentValues.Remove('scheduledActionsForRule')
-$ValuesToCheck.Remove('scheduledActionsForRule')
-$PSBoundParameters.Remove('scheduledActionsForRule')
-#END EXPERIMENTS
-
-    $testResult = $true
-    if ($CurrentValues.Ensure -ne $Ensure)
-    {
-        $testResult = $false
-    }
-    #region Assignments
-    if ($testResult)
-    {
-        $source = Get-M365DSCDRGComplexTypeToHashtable -ComplexObject $PSBoundParameters.Assignments
-        $target = $CurrentValues.Assignments
-        $testResult = Compare-M365DSCIntunePolicyAssignment -Source $source -Target $target
-        $ValuesToCheck.Remove('Assignments') | Out-Null
-    }
-    #endregion
-
-    #Compare Cim instances
-    foreach ($key in $PSBoundParameters.Keys)
-    {
-        $source = $PSBoundParameters.$key
-        $target = $CurrentValues.$key
-        if ($source.getType().Name -like '*CimInstance*')
-        {
-            $testResult = Compare-M365DSCComplexObject `
-                -Source ($source) `
-                -Target ($target) -Verbose
-
-            if (-Not $testResult)
-            {
-                Write-Verbose -Message "Drift detected for the complex object key: $key"
-                $testResult = $false
-                break
-            }
-
-            $ValuesToCheck.Remove($key) | Out-Null
-        }
-    }
-
-    if ($testResult)
-    {
-        $TestResult = Test-M365DSCParameterState -CurrentValues $CurrentValues `
-            -Source $($MyInvocation.MyCommand.Source) `
-            -DesiredValues $PSBoundParameters `
-            -ValuesToCheck $ValuesToCheck.Keys
-    }
-    Write-Verbose -Message "Test-TargetResource returned $TestResult"
-
-    return $TestResult
-}
-#>
-
     #Ensure the proper dependencies are installed in the current environment.
     Confirm-M365DSCDependencies
 
@@ -981,8 +888,7 @@ $PSBoundParameters.Remove('scheduledActionsForRule')
 
     $CurrentValues = Get-TargetResource @PSBoundParameters
     $ValuesToCheck = ([Hashtable]$PSBoundParameters).clone()
-    $PSBoundParameters | OUt-File "C:\dsc-5592\BoundParams.txt"
-    $ValuesToCheck | OUt-File "C:\dsc-5592\ValuesToCheck.txt"
+
     if ($CurrentValues.Ensure -ne $Ensure)
     {
         Write-Verbose -Message "Test-TargetResource returned $false"
@@ -990,16 +896,10 @@ $PSBoundParameters.Remove('scheduledActionsForRule')
     }
     $testResult = $true
 
-
-#$CurrentValues.Remove('scheduledActionsForRule')
-#$ValuesToCheck.Remove('scheduledActionsForRule')
-#$PSBoundParameters.Remove('scheduledActionsForRule')
     #Compare Cim instances
     foreach ($key in $PSBoundParameters.Keys)
     {
         $source = $PSBoundParameters.$key
-        $source | Out-File "C:\dsc-5592\source\$key.txt"
-        $target | Out-File "C:\dsc-5592\target\$key.txt"
         $target = $CurrentValues.$key
         if ($source.getType().Name -like '*CimInstance*')
         {
